866305d3f40544240f1b4a1ef5684cba7a2d1c56
Jason Manning <jasonkylemanning@gmail.com>
2022-03-08 20:27:48 -0500

    update close modal, next img, prev img modal functions

--- app.js
+++ app.js
@@ -14,11 +14,16 @@ function Gallery(element) {
   
   this.modal = getElement('.modal');
   this.modalImg = getElement('.main-img');
-  this.imageName = getElement('image-name');
+  this.imageName = getElement('.image-name');
   this.modalImages = getElement('.modal-images');
   this.closeBtn = getElement('.close-btn');
-  this.nextBtn = getElement(".next-btn");
-  this.prevBtn = getElement(".prev-btn");
+  this.nextBtn = getElement('.next-btn');
+  this.prevBtn = getElement('.prev-btn');
+
+  this.closeModal = this.closeModal.bind(this);
+  this.nextImage = this.nextImage.bind(this);
+  this.prevImage = this.prevImage.bind(this);
+  this.modalImages = this.chooseImage.bing(this);
 
   this.containter.addEventListener('click', function (e) {
     if (e.target.classList.contains('img')) {
@@ -29,12 +34,47 @@ function Gallery(element) {
 
 Gallery.prototype.openModal = function (selectedImage, list) {
   this.setMainImage(selectedImage);
+  this.modalImages.innerHTML = list.map(function(image) {
+    return `<img src="${image.src}" title="${image.title}" data-id="${image.dataset.id}" class="${selectedImage.dataset.id === image.dataset.id ? 'modal-img selected' : 'modal-img'}" />`
+  }).join('')
   this.modal.classList.add('open');
+  this.closeBtn.addEventListener('click', this.closeModal);
+  this.nextBtn.addEventListener('click', this.nextImage);
+  this.prevBtn.addEventListener('click', this.prevImage);
+  this.modalImages.addEventListener('click', this.chooseImage);
 }
 
 Gallery.prototype.setMainImage = function (selectedImage) {
   this.modalImg.src = selectedImage.src;
   this.imageName.textContent = selectedImage.title;
+}
+
+Gallery.prototype.closeModal = function () {
+  this.modal.classList.remove('open');
+  this.closeBtn.removeEventListener('click', this.closeModal);
+  this.nextBtn.removeEventListener('click', this.nextImage);
+  this.prevBtn.removeEventListener('click', this.prevImage);
+  this.modalImages.removeEventListener('click', this.chooseImage);
+}
+
+Gallery.prototype.nextImage = function () {
+  const selected = this.modalImages.querySelector('.selected');
+  const next = selected.nextElementSibling || this.modalImages.firstElementChild;
+  selected.classList.remove('selected');
+  next.classList.add('selected');
+  this.setMainImage(next);
+}
+
+Gallery.prototype.prevImage = function () {
+  const selected = this.modalImages.querySelector(".selected");
+  const prev = selected.previousElementSibling || this.modalImages.lastElementChild;
+  selected.classList.remove('selected');
+  prev.classList.add('selected');
+  this.setMainImage(prev);
+}
+
+Gallery.prototype.chooseImage = function (e) {
+  
 }
 
 const nature = new Gallery(getElement('.nature'));
