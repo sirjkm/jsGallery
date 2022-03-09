19b5a50f9484e95347a81bc0ab6106ba98f779ef
Jason Manning <jasonkylemanning@gmail.com>
2022-03-08 21:11:39 -0500

    update thumbnail functionality

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ function Gallery(element) {
   this.closeModal = this.closeModal.bind(this);
   this.nextImage = this.nextImage.bind(this);
   this.prevImage = this.prevImage.bind(this);
-  this.modalImages = this.chooseImage.bing(this);
+  this.chooseImage = this.chooseImage.bind(this);
 
   this.containter.addEventListener('click', function (e) {
     if (e.target.classList.contains('img')) {
@@ -74,7 +74,13 @@ Gallery.prototype.prevImage = function () {
 }
 
 Gallery.prototype.chooseImage = function (e) {
-  
+  if (e.target.classList.contains('modal-img')) {
+    const selected = this.modalImages.querySelector('.selected');
+    selected.classList.remove('.selected');
+
+    this.setMainImage(e.target);
+    e.target.classList.add('.selected');
+  }
 }
 
 const nature = new Gallery(getElement('.nature'));
